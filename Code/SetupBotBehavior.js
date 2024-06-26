f676180f38f435c0e1e5098e21ca1381d981d6fc
pippybot <150129081+pippybot@users.noreply.github.com>
2024-06-26 18:05:26 -0500

    Update SetupBotBehavior.js

--- Code/SetupBotBehavior.js
+++ Code/SetupBotBehavior.js
@@ -1,13 +1,14 @@
 private setupBotBehavior() {
     this.twitchClient.on('message', (channel: any, tags: any, message: any, self: any) => {
-        let helloCommand = "!hello"
+        let helloCommand = "Hey there, $(user)! Welcome to the stream! Please make sure to read the rules by typing '!rules'."
 
         // Ignore messages from itself.
         if (self) return;
 
         //! means a command is coming by, and we check if it matches the command we currently support
-        if (message.startsWith('!') && message === helloCommand)
+        if (message.startsWith('pineapple pizza') or ('Pineapple pizza') or ('Pineapple Pizza') && message === helloCommand)
             this.sayHelloToUser(channel,tags);
+
     });
 }
 
