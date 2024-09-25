f9d44256862816bffbb059180dd4dcd57a5effe6
pippybot <150129081+pippybot@users.noreply.github.com>
2024-09-25 12:49:02 -0500

    Update SetupBotBehavior.js
    edited with proper closings and spacings

--- Code/SetupBotBehavior.js
+++ Code/SetupBotBehavior.js
@@ -1,18 +1,17 @@
 private setupBotBehavior() {
     this.twitchClient.on('message', (channel: any, tags: any, message: any, self: any) => {
-        let helloCommand = "Hey there, $(user)! Welcome to the stream! Please make sure to read the rules by typing '!rules'."
+        const helloCommand = "Hey there, $(user)! Welcome to the stream! Please make sure to read the rules by typing '!rules'.";
 
         // Ignore messages from itself.
         if (self) return;
 
         //! means a command is coming by, and we check if it matches the command we currently support
-        if (message.startsWith('pineapple pizza') or ('Pineapple pizza') or ('Pineapple Pizza') && message === helloCommand)
-            this.sayHelloToUser(channel,tags);
-
+        if ((message.startsWith('pineapple pizza') || message.startsWith('Pineapple pizza') || message.startsWith('Pineapple Pizza') && message === helloCommand) {
+            this.sayHelloToUser(channel, tags);
+        }
     });
 }
 
 private sayHelloToUser(channel: any, tags: any) {
-    this.twitchClient.say(channel, `Hello, ${ tags.username }! Welcome to the channel.`);
-    })
+    this.twitchClient.say(channel, `Hello, ${tags.username}! Welcome to the channel.`);
 }
